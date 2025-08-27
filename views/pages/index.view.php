930a794c115c42beb100da02742d257c690179a0
Rammah Karpous <rammahkarpous@outlook.com>
2025-08-27 09:29:49 +0200

    chore: moved views into a pages sub-directory

--- views/pages/index.view.php
+++ views/pages/index.view.php
@@ -5,4 +5,9 @@
 
     <h1>Hello from pages/index view</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis repellat odit reprehenderit aut doloribus eligendi porro aperiam neque eum ullam facilis beatae rem hic eos cumque soluta, tempora et consequuntur quasi nam unde sunt nihil ipsam? Asperiores enim quia et saepe? Voluptate nihil ab dicta enim magni? Suscipit, magnam nemo?</p>
+
+    <?php component('card', [
+        'title' => 'Button component',
+        'buttonText' => 'Click the button'
+    ]) ?>
 </div>
